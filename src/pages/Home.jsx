e0a84e9bb677724fd5f4c11926f7f16f1422336b
parvindar <parvindarsingh43958@gmail.com>
2025-05-31 22:28:29 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,17 @@ const Home = () => {
   return (
 
     <>
-    <div><h1>Welcome to our Loan Application</h1>
-      <p>Click to <Link to="/signup">Signup</Link> or Already a user? <Link to="/login">Login</Link></p>
-    </div>
+      <div className="container d-flex flex-column align-items-center justify-content-center vh-100 text-center">
+        <div className="card p-5 shadow-lg">
+          <h1 className="mb-4">Welcome to Our Loan Application</h1>
+          <p className="lead">
+            New here? <Link to="/signup" className="btn btn-primary mx-2">Sign Up</Link>
+            or
+            <span className="mx-2">Already a user?</span>
+            <Link to="/login" className="btn btn-outline-primary">Log In</Link>
+          </p>
+        </div>
+      </div>
       <Outlet />
     </>
 
